8a9a57030e2299e15d84cf7b1241ecb579d091e1
ShadowArcanist <162910371+ShadowArcanist@users.noreply.github.com>
2026-02-04 14:16:06 +0100

    Allow CORS from everywhere

--- src/server.js
+++ src/server.js
@@ -11,6 +11,16 @@ class Server {
 
   createServer() {
     return http.createServer((req, res) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+      if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
+      }
+
       const { url } = req;
 
       if (url === '/') {
